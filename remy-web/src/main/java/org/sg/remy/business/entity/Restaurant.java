a49457825f600b5f2235bc5bf62a5bbf2bf1b54e
Semyon Goryachkin <artful.tiger@gmail.com>
2012-08-07 23:42:46 +0400

    Добавлена аннотация Basic к большому текстовому полю

--- remy-web/src/main/java/org/sg/remy/business/entity/Restaurant.java
+++ remy-web/src/main/java/org/sg/remy/business/entity/Restaurant.java
@@ -3,8 +3,10 @@ package org.sg.remy.business.entity;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Basic;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,6 +24,7 @@ public class Restaurant implements Serializable{
 	private String name;
 
 	@Lob
+	@Basic(fetch=FetchType.EAGER)
 	private String description;
 	
 	@Embedded
