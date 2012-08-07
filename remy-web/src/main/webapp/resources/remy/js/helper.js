be4711b947f74ae12ad5f4567f1d261442f69d29
Semyon Goryachkin <sgoryachkin@aplana.com>
2012-08-07 12:36:02 +0400

    Эксперементы с crossDomain

--- remy-web/src/main/webapp/resources/remy/js/helper.js
+++ remy-web/src/main/webapp/resources/remy/js/helper.js
@@ -1,29 +1,33 @@
-var picasa = function(userId, albomId, photolistener) {
+var picasa = function(userId, albomId, photoListener) {
+
+	var parseData = function(data) {
+		$.each(data.feed.entry, function(i, item) {
+			$.each(item.media$group.media$content, function(i, item) {
+				photoUrl = item.url;
+			});
+			$.each(item.media$group.media$thumbnail, function(i, item) {
+				photoThumb = item.url;
+			});
+
+			photo = photoUrl.split('/')[7];
+			photoUrl = photoUrl.replace(photo, 's1024/' + photo);
+
+			photoTitle = item.media$group.media$title.$t;
+			if (photoTitle == photo)
+				photoTitle = '';
+
+			photoListener(photoUrl, photoTitle, photoThumb);
+		});
+	};
+
 	$.ajax({
 		type : 'GET',
-		url : 'http://picasaweb.google.com/data/feed/base/user/' + userId
+		url : 'https://picasaweb.google.com/data/feed/base/user/' + userId
 				+ '/albumid/' + albomId + '?alt=json&thumbsize=80',
-		success : function(data) {
-			$.each(data.feed.entry, function(i, item) {
-				$.each(item.media$group.media$content, function(i, item) {
-					photoUrl = item.url;
-				});
-				$.each(item.media$group.media$thumbnail, function(i, item) {
-					photoThumb = item.url;
-				});
-
-				photo = photoUrl.split('/')[7];
-				photoUrl = photoUrl.replace(photo, 's800/' + photo);
-
-				photoTitle = item.media$group.media$title.$t;
-				if (photoTitle == photo)
-					photoTitle = '';
-
-				photolistener(photoUrl, photoTitle, photoThumb)
-			});
-		},
-		crossDomain: true,
+		crossDomain : true,
 		dataType : 'json',
 		async : false,
+		success : parseData
 	});
-}
+
+};
