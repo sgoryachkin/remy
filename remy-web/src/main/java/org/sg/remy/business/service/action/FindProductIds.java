288efb656f881d3bc32bbced33b238a1cfe47b83
Semyon Goryachkin <sgoryachkin@aplana.com>
2012-09-04 17:54:54 +0400

    Добавлена команда FindProductIds

---
+++ remy-web/src/main/java/org/sg/remy/business/service/action/FindProductIds.java
@@ -0,0 +1,55 @@
+package org.sg.remy.business.service.action;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.sg.remy.business.model.PagingParam;
+import org.sg.remy.business.model.PagingResult;
+import org.sg.remy.business.model.ProductFilter;
+import org.sg.remy.common.command.api.Action;
+
+public class FindProductIds implements Serializable, Action<PagingResult<Long>>{
+	private static final long serialVersionUID = -1949958005718397244L;
+	
+	private PagingParam<ProductFilter> pagingParam;
+	
+	public FindProductIds(){}
+	
+	public FindProductIds(PagingParam<ProductFilter> pagingParam){
+		this.pagingParam = pagingParam;
+	}
+	
+	public PagingParam<ProductFilter> getPagingParam() {
+		return pagingParam;
+	}
+
+	public void setPagingParam(PagingParam<ProductFilter> pagingParam) {
+		this.pagingParam = pagingParam;
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(pagingParam).toHashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (obj == this)
+			return true;
+		if (obj.getClass() != getClass())
+			return false;
+		FindProductIds rhs = (FindProductIds) obj;
+        return new EqualsBuilder().append(this.pagingParam, rhs.pagingParam).isEquals();
+	}
+	
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this.pagingParam).toString();
+	}
+	
+	
+}
---
+++ remy-web/src/main/java/org/sg/remy/business/service/handler/FindProductIdsHandler.java
@@ -0,0 +1,70 @@
+package org.sg.remy.business.service.handler;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+
+import org.sg.remy.business.entity.Product;
+import org.sg.remy.business.entity.ProductCategory;
+import org.sg.remy.business.entity.Product_;
+import org.sg.remy.business.model.PagingParam;
+import org.sg.remy.business.model.PagingResult;
+import org.sg.remy.business.model.PagingResultImpl;
+import org.sg.remy.business.model.ProductFilter;
+import org.sg.remy.business.service.action.FindProductIds;
+import org.sg.remy.business.util.PagingCriteriaUtils;
+import org.sg.remy.common.command.api.ActionHandler;
+import org.sg.remy.common.command.api.Handler;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+@Handler(action = FindProductIds.class)
+public class FindProductIdsHandler implements
+		ActionHandler<FindProductIds, PagingResult<Long>> {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public PagingResult<Long> execute(FindProductIds command) {
+		PagingParam<ProductFilter> param = command.getPagingParam();
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+
+		Root<Product> fromProduct = cq.from(Product.class);
+		Join<Product, ProductCategory> fromProductCategories = fromProduct
+				.join(Product_.categories);
+		
+		// Restrictions
+		cq.where(fromProductCategories.get(Product_.id).in(
+				param.getFilter().getCategoryIds()));
+
+		PagingResultImpl<Long> pagingResult = new PagingResultImpl<Long>();
+		pagingResult.setPage(param.getPage());
+
+		// Set selection for count 
+		cq.select(cb.count(fromProduct));
+		pagingResult.setCount(em.createQuery(cq).getSingleResult());
+		pagingResult.setPageCount(PagingCriteriaUtils.calcPageCount(
+				pagingResult.getCount(), param.getPageSize()));
+
+		// Set selection for ids
+		cq.select(fromProduct.get(Product_.id)).distinct(true);
+		pagingResult.setResult(em
+				.createQuery(cq)
+				.setFirstResult(
+						PagingCriteriaUtils.calcFirstResult(
+								param.getPageSize(), param.getPage())
+								.intValue())
+				.setMaxResults(((Long) (param.getPageSize())).intValue())
+				.getResultList());
+		return pagingResult;
+	}
+
+}
